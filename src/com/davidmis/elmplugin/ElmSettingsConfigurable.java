f39051263751fb4ea90d956d3d585269f59683ee
David Mis <d.mis09@gmail.com>
2015-01-26 22:39:21 -0600

    Added Settings page, but haven't persisted state yet.

---
+++ src/com/davidmis/elmplugin/ElmSettingsConfigurable.java
@@ -0,0 +1,60 @@
+package com.davidmis.elmplugin;
+
+import com.intellij.openapi.options.Configurable;
+import com.intellij.openapi.options.ConfigurationException;
+import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+
+/**
+ * Created by david on 1/26/15.
+ */
+public class ElmSettingsConfigurable implements Configurable {
+    private ElmSettingsComponent component;
+
+    @Nls
+    @Override
+    public String getDisplayName() {
+        return "Elm language";
+    }
+
+    @Nullable
+    @Override
+    public String getHelpTopic() {
+        return "Elm language";
+    }
+
+    @Nullable
+    @Override
+    public JComponent createComponent() {
+        if (component == null) {
+            component = new ElmSettingsComponent();
+        }
+        return component.mainPanel;
+    }
+
+    @Override
+    public boolean isModified() {
+        return true;
+    }
+
+    @Override
+    public void apply() throws ConfigurationException {
+        if(component == null || component.pathField.getText() == null) {
+            return;
+        }
+
+        System.out.println("Path to elm-make: " + component.pathField.getText());
+    }
+
+    @Override
+    public void reset() {
+
+    }
+
+    @Override
+    public void disposeUIResources() {
+
+    }
+}
